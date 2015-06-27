e96efa44476c2762a9c8c60228d642a26099e1b3
Daniel de la Concha <Daniel@ddlc-mac.attlocal.net>
2015-06-27 16:18:48 -0500

    added test class

---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Daniel de la Concha on 6/27/15.
+//  Copyright (c) 2015 unotinto. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
